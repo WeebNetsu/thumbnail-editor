8a69d7583306938d679ea5c1f1204692c2f03749
WeebNetsu <stephenvdw7777@gmail.com>
2021-07-25 15:36:33 +0200

    New flag system.
    New way to use flags, so '--flag value' and not '--flag:value'. Also, no more OOP.

--- README.md
+++ README.md
@@ -7,33 +7,30 @@ A thumbnail editor for influencers to speed up the creation process!
 This is for people who need to create new thumbnails from templates they have already made and just need to change the text every time.
 
 ## How does this work?
-It's as basic as opening the terminal and executing the program with arguments/flags. Flags (except for the project) is in this format: **--flag-name:flag-value**. This is a working example you can use: `python main.py --python --image:python.jpg --fs:60 --x:415 --y:405 --text:"Part 43:" --font:ubuntu/Ubuntu-Bold --text:"Lambda Functions" --x:455 --y:475 --fs:90 --font:ubuntu/Ubuntu-BoldItalic`
+It's as basic as opening the terminal and executing the program with arguments/flags. Flags is mainly in this format: **--flag-name value1 value2**. For help you can do the following: `python main.py --help`, or read the code, it's not a lot. This is a working example you can use: `python main.py python.jpg -fs 60 90 -x 415 455 -y 405 475 --text "Part 43:" "Lambda Functions" --font ubuntu/Ubuntu-Bold ubuntu/Ubuntu-BoldItalic`
 
 A breakdown of the above:
 
 `python main.py` - execute the program
 
-`--python` - the project that should be used (currently you should add a basic template to your thumbnails.te file to create a new project, this will later be used for defaults)
+<!-- `--python` - the project that should be used (currently you should add a basic template to your thumbnails.te file to create a new project, this will later be used for defaults) -->
 
-`--image:python.jpg` - the image that should be used (location: *base-thumbnails/python.jpg*)
+`python.jpg` - the image that should be used (location: *base-thumbnails/python.jpg*)
 
-`--fs:60` - set the font size to 60
+`-fs 60` - set the font size to 60 for first text and 90 for the rest
 
-`--x:415 --y:405` - the x & y position of the text that should be added
+`-x 415 455 -y 405 475` - the x & y position of the text that should be added (first string of text will be at location: 415(x) 405(y))
 
-`--text:"Part 43:"` - the text that should be added. If you want text to span more than 1 word, then **" "** is required.
+`--text "Part 43:" "Lambda Functions"` - the text that should be added. If you want text to span more than 1 word, then **" "** is required.
 
-`--font:ubuntu/Ubuntu-Bold` - the font family that should be used (can be found inside the fonts/ folder, you can add your own as long as it is in .ttf format :D)
-
-`--text:"Lambda Functions" --x:455 --y:475 --fs:90 --font:ubuntu/Ubuntu-BoldItalic` - same as above, but for 2nd line of text
-The "--" for each flag is optional, but recommended.
+`--font ubuntu/Ubuntu-Bold ubuntu/Ubuntu-BoldItalic` - the font family that should be used (can be found inside the fonts/ folder, you can add your own as long as it is in .ttf format :D), the first piece of text will be in Ubuntu-Bold and all the rest in Ubuntu-BoldItalic
 
 ## Python Requirements
 - PIL
 - termcolor
 
+----------
+
 ## Plans for the future
 - Have a config file for program configs and a config file for thumbnail configs.
 - Have a UI for less tech-savvy people.
-- Add the ability to add line-breaks in text without needing to create new text
-- Better README
--- main.py
+++ main.py
@@ -1,110 +1,133 @@
+import argparse
+
 from PIL import Image, ImageDraw, ImageFont
-import sys
 from termcolor import colored
 
-class ThumbnailEditor():
-    def __init__(self, argv: list):
-        """
-        This will take in argv (usually provided by sys.argv being passed in), the argv is an list that should look something
-        like this: ["--text:Hello World", "--x:500", "--y:300"]. This will clean the inputs, set & check the properties and
-        get project data from thumbnails.te file.
-        """
-        self.argv = argv
-        self.flags = ("image", "text", "fs", "font", "x", "y", "output")
-        self.inputs, self.project = self.cleanInput()
-        self.projects_data = self.getThumbailConfig(self.project)
-        self.properties = self.fillProperties(self.inputs)
+"""
+RULES:
+Below is a working example:
+python main.py python.jpg -fs 60 90 -x 415 455 -y 405 475 --text "Part 43:" "Lambda Functions" --font ubuntu/Ubuntu-Bold ubuntu/Ubuntu-BoldItalic
 
-        if self.projects_data == "":
-            self.giveError(f"Project '{self.project}' not found in thumbnails.te")
+Explaination:
+python -> the python program
+main.py -> the file
+python.jpg -> image that should be modified
+-fs 60 90 -> font size 60 and 90 should be used
+-x 415 455 -> x positions 415 and 455 should be used
+-y 405 475 -> y positions 405 and 475 should be used
+--text "Part 43:" "Lambda Functions" -> The text that should be added to the image
+--font ubuntu/Ubuntu-Bold ubuntu/Ubuntu-BoldItalic -> fonts that should be used
 
-        # self.default_inputs, _ = self.cleanInput(self.projects_data)
-        # self.default_properties = self.fillProperties(self.default_inputs)
-        # print(self.default_properties)
+1. There has to be ONE image selected and it has to be the FIRST parameter
+2. There should be an equal amount of x, y and text arguments
+3. Text seperated by spaces should be in ""
 
-    def cleanInput(self, args = []) -> tuple:
-        """
-        Cleans input flags provided by user when running the program (usually sys.argv passed into constructor), removes '--' and split args at ':'
-        -- is optional in flags, but recommended!
-        """
-        
-        inputs = [arg.replace("--", "").split(":") for arg in args]
-        # print(inputs)
-        project = ""
+Complex example:
+python main.py python.jpg -fs 60 90 -x 415 455 -y 405 475 --text "Part 43:" "Lambda Functions" --font ubuntu/Ubuntu-Bold ubuntu/Ubuntu-BoldItalic -tc 255 255 255 -tc 0 0 0 -bw 1 2 -bc 255 0 0 -bc 0 255 0
+"""
 
-        if not args: # if no custom arguments were passed in
-            inputs = [arg.replace("--", "").split(":") for arg in self.argv]
-            inputs.pop(0) # remove executed python file
-            project = inputs[0][0]
-            inputs.pop(0) # project
-            
-        return (inputs, project)
+# TODO: no-border, output, project, config file
+
+parser = argparse.ArgumentParser() # define the parser
+parser.add_argument("image", help="The image that should be modified.", type=str, metavar='')
+# nargs -> allows us to take in multiple values with the same flag eg. -t "hello world" "I am cool" -> ["hello world", "I am cool"]
+parser.add_argument("--text", "-t", help="The text that should be added to the image.", type=str, metavar='', nargs="+")
+parser.add_argument("--font-size", "-fs", help="Size the font should be.", type=int, metavar='', nargs="+")
+parser.add_argument("--font", "-f", help="The font that should be used (font-family)", type=str, metavar='', nargs="+")
+parser.add_argument("--x-pos", "-x", help="The x position the text should be at on the image.", type=int, metavar='', default=0, nargs="+")
+parser.add_argument("--y-pos", "-y", help="The y position the text should be at on the image.", type=int, metavar='', default=0, nargs="+")
+parser.add_argument("--text-color", "-tc", help="The color of the text in RGB format. eg. -tc 0 100 255", type=int, metavar='', nargs="+", action="append")
+parser.add_argument("--border-color", "-bc", help="The color of the border in Tuple-RGB format. eg. -bc 0 100 255", type=int, metavar='', nargs="+", action="append")
+parser.add_argument("--border-width", "-bw", help="The width (size) of text border", type=int, metavar='', nargs="+")
+parser.add_argument("--no-border", help="Wether a border should be added round text.", action="store_true")
+parser.add_argument("--output", "-o", help="The output location of the image.", type=str, metavar='')
+args = parser.parse_args()
+
+print(args)
+
+def giveError(message: str) -> None:
+    """Display error message and exits program"""
+    print(colored(f"Error: {message}", 'red'))
+    exit()
+
+def giveWarning(message: str) -> None:
+    """Display warning message"""
+    print(colored(f"Warning: {message}", 'yellow'))
+
+def drawText(draw, text, font, font_size, x, y, text_color, border_width, border_color) -> None:
+    """This draws the text to the image, draw and text are required inputs, the rest are optional"""
+    drawFont = ImageFont.truetype(f"fonts/{font}.ttf", int(font_size))
+
+    draw.text(
+        (int(x), int(y)),  # Coordinates
+        text,  # Text
+        text_color,  # Color
+        font=drawFont,
+        stroke_width=border_width, 
+        stroke_fill=border_color
+    )
     
-    def giveError(self, message: str) -> None:
-        """Display error message and exits program"""
-        print(colored(f"Error: {message}", 'red'))
-        sys.exit()
+def createThumbnail() -> None:
+    """This will create the thumbnail"""
+    try:
+        imgFace = Image.open(f"base-thumbnails/{args.image}")
+    except FileNotFoundError:
+        giveError(f"Image {args.image} could not be found inside the 'base-thumbnails' directory.")
 
-    def giveWarning(self, message: str) -> None:
-        """Display warning message"""
-        print(colored(f"Warning: {message}", 'yellow'))
+    draw = ImageDraw.Draw(imgFace)
+    count = 0
+    for text, x_pos, y_pos in zip(args.text, args.x_pos, args.y_pos):
+        try:
+            font_size = args.font_size[count]
+        except IndexError:
+            font_size = args.font_size[-1]
+        except TypeError:
+            font_size = 60
+        
+        try:
+            font_family = args.font[count]
+        except IndexError:
+            font_family = args.font[-1]
+        except TypeError:
+            font_family = "open_sans/OpenSans-Regular"
+        
+        try:
+            text_color = tuple(args.text_color[count])
+        except IndexError:
+            text_color = tuple(args.text_color[-1])
+        except TypeError:
+            text_color = (0, 0, 0)
+            
+        try:
+            border_width = args.border_width[count]
+        except IndexError:
+            border_width = args.border_width[-1]
+        except TypeError:
+            border_width = 2
+        
+        try:
+            border_color = tuple(args.border_color[count])
+        except IndexError:
+            border_color = tuple(args.border_color[-1])
+        except TypeError:
+            border_color = (255, 255, 255)
+        
+        drawText(draw=draw, text=text, x=x_pos, y=y_pos, text_color=text_color, font=font_family, font_size=font_size, border_width=border_width, border_color=border_color)
 
-    def fillProperties(self, inputs: list) -> dict:
-        """
-        Takes in list of cleaned flags (check cleanInput()) and returns a dict that has all the properties from the cleaned input and defaults
-        from thumbnails.te
-        """
-        props = {"image": "", "text": [], "font_size": [], "font_family": [], "x": [], "y": [], "output": ""}
+        count += 1
+    imgFace.show()
 
-        # print(self.projects_data)
+if __name__ == '__main__':
+    if (len(args.x_pos) != len(args.y_pos)) or (len(args.x_pos) != len(args.text)):
+        giveWarning("x, y and text does not have the same amount of arguments.")
+        
+    createThumbnail()
 
-        for arg in inputs:
-            if arg[0] in self.flags:
-                if arg[0] == "image":
-                    props["image"] = arg[1]
-                    
-                if arg[0] == "fs":
-                    props["font_size"].append(arg[1])
+# python main.py --python --image:python.jpg --fs:60 --x:415 --y:405 --text:"Part 43:" --font:ubuntu/Ubuntu-Bold --text:"Lambda Functions" --x:455 --y:475 --fs:90 --font:ubuntu/Ubuntu-BoldItalic
 
-                if arg[0] == "font":
-                    props["font_family"].append(arg[1])
-                    
-                if arg[0] == "text":
-                    props["text"].append(arg[1])
-                    
-                if arg[0] == "x":
-                    props["x"].append(arg[1])
-
-                if arg[0] == "y":
-                    props["y"].append(arg[1])
-                
-                if arg[0] == "no-default":
-                    props["load_defaults"] = False
-                
-                if arg[0] == "output":
-                    props["output"] = arg[1]
-
-            else:
-                self.giveError(f"Flag '{arg[0]}' does not exist")
-
-        if props["x"] or props["y"]: # if they specify x or y positions
-            if len(props["text"]) != len(props["x"]) or len(props["text"]) != len(props["y"]):
-                self.giveError("--text, --x and --y should be used the same amount of times")
-
-        # if not((props["x"] or props["y"]) and props["load_defaults"]) : # if they dont specify x or y positions and said not to load the defaults
-        #     if len(props["text"]) != len(props["x"]) or len(props["text"]) != len(props["y"]):
-        #         self.giveError("--text, --x and --y should be used the same amount of times or load defaults file")
-
-        # if not props["load_defaults"]:
-        #     self.giveWarning("Not loading defaults from file")
-        #     return props
-
-        return props        
-
-    def getThumbailConfig(self, project: str) -> str:
-        """
-        This will get the config for the specific project entered
-        """
+"""
+def getThumbailConfig(self, project: str) -> str:
+        # This will get the config for the specific project entered
         with open("thumbnails.te", "r") as tFile:
             thumbnails = tFile.readlines()
 
@@ -122,54 +145,4 @@ class ThumbnailEditor():
                         return texts
         
         return ""
-
-    def drawText(self, draw, text, x=0, y=0, color=(0,0,0), font="open_sans/OpenSans-Regular", font_size=30, border_width=0, border_color=(255, 255, 255)) -> None:
-        """This draws the text to the image, draw and text are required inputs, the rest are optional"""
-        drawFont = ImageFont.truetype(f"fonts/{font}.ttf", int(font_size))
-
-        draw.text(
-            (int(x), int(y)),  # Coordinates
-            text,  # Text
-            color,  # Color
-            font=drawFont,
-            stroke_width=border_width, 
-            stroke_fill=border_color
-        )
-
-    def createThumbnail(self) -> None:
-        """This will create the thumbnail"""
-        try:
-            imgFace = Image.open(f"base-thumbnails/{self.properties['image']}")
-        except FileNotFoundError:
-            self.giveError(f"Image {self.properties['image']} could not be found inside the 'base-thumbnails' directory.")
-
-        draw = ImageDraw.Draw(imgFace)
-        count = 0
-        for text, x_pos, y_pos in zip(self.properties["text"], self.properties["x"], self.properties["y"]):
-            try:
-                font_size = self.properties["font_size"][count]
-            except IndexError:
-                try:
-                    font_size = self.properties["font_size"][-1]
-                except IndexError:
-                    font_size = 60
-            
-            try:
-                font_family = self.properties["font_family"][count]
-            except IndexError:
-                try:
-                    font_family = self.properties["font_family"][-1]
-                except IndexError:
-                    font_family = "open_sans/OpenSans-Regular"
-            
-            self.drawText(draw=draw, text=text, x=x_pos, y=y_pos, color=(0, 0, 0), font=font_family, font_size=font_size, border_width=2, border_color=(255, 255, 255))
-
-            count += 1
-        imgFace.show()
-
-
-if __name__ == '__main__':
-    # A working command:
-    # python main.py --python --image:python.jpg --fs:60 --x:415 --y:405 --text:"Part 43:" --font:ubuntu/Ubuntu-Bold --text:"Lambda Functions" --x:455 --y:475 --fs:90 --font:ubuntu/Ubuntu-BoldItalic
-    thumb = ThumbnailEditor(sys.argv)
-    thumb.createThumbnail()
+"""
